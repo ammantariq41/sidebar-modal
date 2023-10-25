effb08dc1689868310abfdc7e6e0de94b03d9938
Mahnoor <mahnoor.tekfirst@gmail.com>
2023-10-25 13:00:27 +0500

    sprint 1

---
+++ src/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FaTimes } from 'react-icons/fa'
+import { social, links } from './data'
+
+const Sidebar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Sidebar
